46ece8b339165a124100e156c49784bc74a007cc
torokattila <torcsiattila93@gmail.com>
2022-04-12 22:10:22 +0200

    created the basic folder structure for the server

---
+++ server/src/index.ts
@@ -0,0 +1,9 @@
+import express from 'express';
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+app.use(express.json());
+
+app.listen(PORT, () => {
+    console.log(`App is listening on PORT ${PORT}`);
+});
