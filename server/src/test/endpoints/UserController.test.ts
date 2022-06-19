5b0b65dabb201cb171e836771d27bc28e9bc2236
torokattila <torcsiattila93@gmail.com>
2022-06-19 22:11:25 +0200

    created tests for update specific user endpoint

--- server/src/test/endpoints/UserController.test.ts
+++ server/src/test/endpoints/UserController.test.ts
@@ -6,6 +6,13 @@ import {
 import { login } from '../../../jest.setup';
 import request from 'supertest';
 import app from '../../app';
+import UserService from 'services/UserService';
+
+const updatedUser = {
+    email: testRegisterData.email,
+    firstname: testRegisterData.firstname,
+    lastname: 'User Edited',
+};
 
 describe(`GET /users/${testRegisterData.id} test`, () => {
     test('Get specific user fails because of invalid UUID', async () => {
@@ -52,5 +59,59 @@ describe(`GET /users/${testRegisterData.id} test`, () => {
         expect(body.email).toEqual(testRegisterData.email);
         expect(body.firstname).toEqual(testRegisterData.firstname);
         expect(body.lastname).toEqual(testRegisterData.lastname);
+    });
+});
+
+describe(`PUT /users/${testRegisterData.id} test`, () => {
+    test('Update specific user fails because of invalid UUID', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put('/users/12345')
+            .set('access_token', token);
+
+        expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+        expect(response.body).not.toEqual(testRegisterData);
+        expect(response.body).toEqual({});
+        expect(response.body).not.toContain(testRegisterData.id);
+    });
+
+    test('Update specific user fails because of email is required in request body', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/${testRegisterData.id}`)
+            .send({
+                firstname: testRegisterData.firstname,
+                lastname: testRegisterData.lastname,
+            })
+            .set('access_token', token);
+
+        const { errors } = response.body;
+
+        expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+        expect(errors).not.toBeUndefined();
+        expect(errors[0]).toBe('email_required');
+    });
+
+    test('Update specific user successfully', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/${testRegisterData.id}`)
+            .send(updatedUser)
+            .set('access_token', token);
+
+        const { body } = response;
+
+        expect(response.status).toBe(StatusCodes.OK);
+        expect(response.body).not.toBeUndefined();
+
+        const storedUser = await UserService.findById(testRegisterData.id);
+
+        expect(body.lastname).toEqual(storedUser.lastname);
     });
 });
