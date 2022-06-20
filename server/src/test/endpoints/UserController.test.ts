19e900e5e36d7918a19477c0c19b0fb33612b892
torokattila <torcsiattila93@gmail.com>
2022-06-20 22:16:18 +0200

    created tests for update password endpoint

--- server/src/test/endpoints/UserController.test.ts
+++ server/src/test/endpoints/UserController.test.ts
@@ -14,6 +14,12 @@ const updatedUser = {
     lastname: 'User Edited',
 };
 
+const updatePasswordData = {
+    currentPassword: 'test',
+    newPassword: 'test2',
+    newPasswordConfirm: 'test2',
+};
+
 describe(`GET /users/${testRegisterData.id} test`, () => {
     test('Get specific user fails because of invalid UUID', async () => {
         const loginResponse = await login(testLoginDatas);
@@ -113,5 +119,121 @@ describe(`PUT /users/${testRegisterData.id} test`, () => {
         const storedUser = await UserService.findById(testRegisterData.id);
 
         expect(body.lastname).toEqual(storedUser.lastname);
+    });
+});
+
+describe(`PUT /users/${testRegisterData.id}/password/update test`, () => {
+    test('Update password fails because of invalid UUID', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/12345/password/update`)
+            .send(updatePasswordData)
+            .set('access_token', token);
+
+        expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+        expect(response.body).toEqual({});
+        expect(response.body).not.toContain(testRegisterData.id);
+    });
+
+    test('Update password fails because of invalid current password', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/${testRegisterData.id}/password/update`)
+            .send({
+                currentPassword: '312fetert5ertg45g',
+                newPassword: updatePasswordData.newPassword,
+                newPasswordConfirm: updatePasswordData.newPasswordConfirm,
+            })
+            .set('access_token', token);
+
+        const { errors } = response.body;
+
+        expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+        expect(errors[0]).toEqual('invalid_current_password');
+        expect(errors).not.toBeUndefined();
+        expect(response.body).not.toContain(testRegisterData.id);
+    });
+
+    test('Update password fails because of newPassword property does not exist in the request body', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/${testRegisterData.id}/password/update`)
+            .send({
+                currentPassword: updatePasswordData.currentPassword,
+                newPasswordConfirm: updatePasswordData.newPasswordConfirm,
+            })
+            .set('access_token', token);
+
+        const { errors } = response.body;
+
+        expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+        expect(errors[0]).toEqual('password_is_required');
+        expect(errors).not.toBeUndefined();
+        expect(response.body).not.toContain(testRegisterData.id);
+    });
+
+    test('Update password fails because of newPasswordConfirm property does not exist in the request body', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/${testRegisterData.id}/password/update`)
+            .send({
+                currentPassword: updatePasswordData.currentPassword,
+                newPassword: updatePasswordData.newPassword,
+            })
+            .set('access_token', token);
+
+        const { errors } = response.body;
+
+        expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+        expect(errors[0]).toEqual('password_confirm_is_required');
+        expect(errors).not.toBeUndefined();
+        expect(response.body).not.toContain(testRegisterData.id);
+    });
+
+    test('Update password fails because newPassword and newPasswordConfirm are not match', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/${testRegisterData.id}/password/update`)
+            .send({
+                currentPassword: updatePasswordData.currentPassword,
+                newPassword: updatePasswordData.newPassword,
+                newPasswordConfirm: 'test3',
+            })
+            .set('access_token', token);
+
+        const { errors } = response.body;
+
+        expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+        expect(errors[0]).toEqual('passwords_must_match');
+        expect(errors).not.toBeUndefined();
+        expect(response.body).not.toContain(testRegisterData.id);
+    });
+
+    test('Update password successfully', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+
+        const response = await request(app)
+            .put(`/users/${testRegisterData.id}/password/update`)
+            .send(updatePasswordData)
+            .set('access_token', token);
+
+        const { id, email, firstname, errors } = response.body;
+
+        expect(response.status).toBe(StatusCodes.OK);
+        expect(errors).toBeUndefined();
+        expect(id).toEqual(testRegisterData.id);
+        expect(email).toEqual(testRegisterData.email);
+        expect(firstname).toEqual(testRegisterData.firstname);
     });
 });
