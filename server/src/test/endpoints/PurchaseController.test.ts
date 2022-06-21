eb53b11bd75cdc540c96255e569c27268ade4485
torokattila <torcsiattila93@gmail.com>
2022-06-21 21:52:55 +0200

    created tests for purchase update endpoint

--- server/src/test/endpoints/PurchaseController.test.ts
+++ server/src/test/endpoints/PurchaseController.test.ts
@@ -32,6 +32,15 @@ const successfulPostedPurchase = {
     modified: new Date(),
 };
 
+const updatedPurchaseData = {
+    id: 'de57018a-431d-4064-b5a1-95891eabdb91',
+    userId: '123e4567-e89b-12d3-a456-426614174000',
+    category: PurchaseCategory.CLOTHING,
+    created: new Date(),
+    modified: new Date(),
+    amount: 10000,
+};
+
 describe('POST /purchases test', () => {
     test('Create purchase fails due to lack of amount', async () => {
         const loginResponse = await login(testLoginDatas);
@@ -176,5 +185,94 @@ describe('GET /purchases test', () => {
         expect(response.status).toBe(StatusCodes.OK);
         expect(response.body).not.toBeUndefined();
         expect(response.body.length).toBe(4);
+    });
+});
+
+describe(`PUT /purchases/${successfulPostedPurchase.id} test`, () => {
+    test('Update purchase fails because purchase id query param is not a UUID', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+        const response = await request(app)
+            .put(`${purchasesUrl}/12345`)
+            .send(updatedPurchaseData)
+            .set('access_token', token);
+
+        expect(response.status).toBe(StatusCodes.INTERNAL_SERVER_ERROR);
+        expect(response.body).not.toContain(successfulPostedPurchase.id);
+        expect(response.body).toEqual({});
+    });
+
+    test('Update purchase fails due to lack of amount', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+        const response = await request(app)
+            .put(`${purchasesUrl}/${successfulPostedPurchase.id}`)
+            .send({
+                id: 'de57018a-431d-4064-b5a1-95891eabdb91',
+                userId: '123e4567-e89b-12d3-a456-426614174000',
+                category: PurchaseCategory.CLOTHING,
+                created: new Date(),
+                modified: new Date(),
+            })
+            .set('access_token', token);
+
+        const { errors } = response.body;
+        expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+        expect(errors[0]).toEqual('amount_required');
+        expect(response.body).not.toContain(successfulPostedPurchase.id);
+
+        // Check if purchase with updated purchase id does not changed
+        const storedPurchase = await PurchaseService.findById(
+            successfulPostedPurchase.id
+        );
+        expect(storedPurchase.amount).toEqual(successfulPostedPurchase.amount);
+    });
+
+    test('Update purchase fails due to lack of category', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+        const response = await request(app)
+            .put(`${purchasesUrl}/${successfulPostedPurchase.id}`)
+            .send({
+                id: 'de57018a-431d-4064-b5a1-95891eabdb91',
+                userId: '123e4567-e89b-12d3-a456-426614174000',
+                amount: 10000,
+                created: new Date(),
+                modified: new Date(),
+            })
+            .set('access_token', token);
+
+        const { errors } = response.body;
+
+        expect(response.status).toBe(StatusCodes.BAD_REQUEST);
+        expect(errors).not.toBeUndefined();
+        expect(errors[0]).toEqual('category_required');
+        expect(response.body).not.toContain(successfulPostedPurchase.id);
+    });
+
+    test('Update purchase successfully', async () => {
+        const loginResponse = await login(testLoginDatas);
+        const token = await loginResponse.body.token;
+        const response = await request(app)
+            .put(`${purchasesUrl}/${successfulPostedPurchase.id}`)
+            .send(updatedPurchaseData)
+            .set('access_token', token);
+
+        const { errors } = response.body;
+        // Check if posted purchase data updated successfully in the db
+        const storedPurchase = await PurchaseService.findById(
+            successfulPostedPurchase.id
+        );
+
+        expect(response.status).toBe(StatusCodes.OK);
+        expect(response.body).not.toBeUndefined();
+        expect(response.body).toEqual(
+            expect.objectContaining({
+                id: successfulPostedPurchase.id,
+            })
+        );
+        expect(errors).toBeUndefined();
+        expect(storedPurchase.amount).toEqual(updatedPurchaseData.amount);
+        expect(storedPurchase.category).toEqual(updatedPurchaseData.category);
     });
 });
